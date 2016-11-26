9f792242e7bc755fdcfaddd9d2e4ff5764b0910a
Vincent De Smet <vincent.desmet@honestbee.com>
2016-11-26 11:00:40 +0700

    Only keep basename of

--- scripts/grademe.sh
+++ scripts/grademe.sh
@@ -1,4 +1,4 @@
 curl -X POST -H "Content-Type: application/json" -d '{
-          "team_name": "${CI_PROJECT_DIR}",
-          "competition_round": "${ROUND}",
-      }' "http://scorer.honestbee.com:5000/submit"
+  "team_name": "$(basename $CI_PROJECT_DIR)",
+  "competition_round": "${ROUND}",
+}' "http://scorer.honestbee.com:5000/submit"
